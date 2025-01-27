b36143060f4567edfc8bde91166d255bc6797f92
Artamananda <artamananda@gmail.com>
2025-01-27 14:07:02 +0700

    add package.json

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	r.ParseMultipartForm(10 << 20)
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	targetDir := "/home/arta"
+	if err := os.MkdirAll(targetDir, os.ModePerm); err != nil {
+		http.Error(w, "Error creating upload directory", http.StatusInternalServerError)
+		return
+	}
+
+	targetFilePath := filepath.Join(targetDir, fileHeader.Filename)
+
+	outFile, err := os.Create(targetFilePath)
+	if err != nil {
+		http.Error(w, "Error creating file", http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, file)
+	if err != nil {
+		http.Error(w, "Error saving the file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "File uploaded successfully and saved to %s", targetFilePath)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/upload", uploadFileHandler)
+	log.Println("Server started on :8001")
+	if err := http.ListenAndServe(":8001", nil); err != nil {
+		log.Fatal(err)
+	}
 }
