6f4f2d322b7d516756194c30dffffd322959e890
Artamananda <artamananda@gmail.com>
2025-01-27 14:09:48 +0700

    add log

--- main.go
+++ main.go
@@ -12,12 +12,14 @@ import (
 func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		log.Println("Invalid request method")
 		return
 	}
 	r.ParseMultipartForm(10 << 20)
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+		log.Println("Error retrieving the file")
 		return
 	}
 	defer file.Close()
@@ -25,6 +27,7 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	targetDir := "/home/arta"
 	if err := os.MkdirAll(targetDir, os.ModePerm); err != nil {
 		http.Error(w, "Error creating upload directory", http.StatusInternalServerError)
+		log.Println("Error creating upload directory")
 		return
 	}
 
@@ -33,6 +36,7 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	outFile, err := os.Create(targetFilePath)
 	if err != nil {
 		http.Error(w, "Error creating file", http.StatusInternalServerError)
+		log.Println("Error creating file")
 		return
 	}
 	defer outFile.Close()
@@ -40,6 +44,7 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	_, err = io.Copy(outFile, file)
 	if err != nil {
 		http.Error(w, "Error saving the file", http.StatusInternalServerError)
+		log.Println("Error saving the file")
 		return
 	}
 
